62e9dafc9b26d1f3303d3a640d777ec75e066170
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-09-10 07:58:18 +0900

    Slider: range編集UIをコンポーネント化

---
+++ src/lib/components/control/Slider.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let value: number
+  export let min: number
+  export let max: number
+  export let step: number
+  export let disabled = false
+  export let onChange: (value: number) => void = () => {}
+</script>
+
+<label>
+  <input
+    type="range"
+    bind:value
+    {min}
+    {max}
+    {step}
+    {disabled}
+    on:change={(e) => onChange(e.currentTarget.valueAsNumber)}
+  />
+  <slot />
+</label>

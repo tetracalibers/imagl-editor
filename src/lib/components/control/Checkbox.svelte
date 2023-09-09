0a6f9897c33f2e8b03572300caa5920501ae7281
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-09-09 17:13:27 +0900

    チェックボックスをコンポーネント化

---
+++ src/lib/components/control/Checkbox.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  let on = false
+  export let onChange: (on: boolean) => void = () => {}
+</script>
+
+<label>
+  <input type="checkbox" bind:checked={on} on:change={(e) => onChange(e.currentTarget.checked)} />
+  <slot />
+</label>

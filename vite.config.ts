93166e3a5e383ccd0f913ebd83030676eda162f2
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-09-09 09:58:08 +0900

    chore(*): setup glslify

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { sveltekit } from "@sveltejs/kit/vite"
 import { defineConfig } from "vite"
+import glslify from "vite-plugin-glslify"
 
 export default defineConfig({
-  plugins: [sveltekit()]
+  plugins: [
+    sveltekit(),
+    glslify({
+      include: ["**/*.glsl", "**/*.vs", "**/*.fs", "**/*.vert", "**/*.frag"]
+    })
+  ]
 })

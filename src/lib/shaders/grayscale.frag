5e04743eccc9c61d77fd2455bafcec678120e267
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-09-10 11:56:01 +0900

    ぼかし機能を追加

--- src/lib/shaders/grayscale.frag
+++ src/lib/shaders/grayscale.frag
@@ -5,17 +5,60 @@ precision highp float;
 #pragma glslify: toGrayscale = require('sketchgl/glsl/grayscale')
 #pragma glslify: toSepia = require('sketchgl/glsl/sepia')
 #pragma glslify: gammaToneCurve = require('sketchgl/glsl/gamma-tone-curve')
+#pragma glslify: gauss = require("sketchgl/glsl/gauss")
+
+// Gaussianフィルタによる平滑化（横方向）
+vec3 xGaussSmooth(sampler2D tex, vec2 uv, vec2 texelSize, float filterSize, float sigma) {
+  float weights = 0.0;
+  vec3 grad = vec3(0.0);
+
+  float h = (filterSize - 1.0) / 2.0;
+
+  for (float i = -h; i <= h; ++i) {
+    float weight = gauss(i, sigma);
+    vec2 offset = vec2(i * texelSize.x, 0.0);
+    vec3 color = texture(tex, uv + offset).rgb;
+    weights += weight;
+    grad += color * weight;
+  }
+
+  return grad / weights;
+}
+
+// Gaussianフィルタによる平滑化（縦方向）
+vec3 yGaussSmooth(sampler2D tex, vec2 uv, vec2 texelSize, float filterSize, float sigma) {
+  float weights = 0.0;
+  vec3 grad = vec3(0.0);
+
+  float h = (filterSize - 1.0) / 2.0;
+
+  for (float i = -h; i <= h; ++i) {
+    float weight = gauss(i, sigma);
+    vec2 offset = vec2(0.0, i * texelSize.y);
+    vec3 color = texture(tex, uv + offset).rgb;
+    weights += weight;
+    grad += color * weight;
+  }
+
+  return grad / weights;
+}
 
 uniform sampler2D uTexture0;
 uniform int uFilterMode;
 // uFilterMode == 3: ガンマ補正によるコントラスト調整
 uniform float uContrastGamma;
+// uFilterMode == 4: x軸方向のぼかし
+// uFilterMode == 5: y軸方向のぼかし
+uniform float uBlurSigma;
 
 in vec2 vTextureCoords;
 
 out vec4 fragColor;
 
 void main() {
+  ivec2 textureSize = textureSize(uTexture0, 0);
+  vec2 texelSize = 1.0 / vec2(float(textureSize.x), float(textureSize.y));
+  
   vec2 uv = vec2(vTextureCoords.x, 1.0 - vTextureCoords.y);
   vec4 smpColor = texture(uTexture0, uv);
 
@@ -25,7 +68,11 @@ void main() {
       ? toSepia(smpColor.rgb)
       : uFilterMode == 3
         ? gammaToneCurve(smpColor.rgb, uContrastGamma)
-        : smpColor.rgb;
+        : uFilterMode == 4
+          ? xGaussSmooth(uTexture0, uv, texelSize, 9.0, uBlurSigma)
+          : uFilterMode == 5
+            ? yGaussSmooth(uTexture0, uv, texelSize, 9.0, uBlurSigma)
+            : smpColor.rgb;
 
   fragColor = vec4(finalColor, 1.0);
 }
--- src/routes/Canvas.svelte
+++ src/routes/Canvas.svelte
@@ -15,20 +15,25 @@
 
   let canvas: HTMLCanvasElement
 
-  type FILTER = "GRAYSCALE" | "SEPIA" | "CONTRAST"
+  type FILTER = "GRAYSCALE" | "SEPIA" | "CONTRAST" | "BLUR_X" | "BLUR_Y"
 
   const FILTER_MODE = {
     NONE: 0,
     GRAYSCALE: 1,
     SEPIA: 2,
-    CONTRAST: 3
-  }
+    CONTRAST: 3,
+    BLUR_X: 4,
+    BLUR_Y: 5
+  } as const
 
   const FILTER_ENABLE_STATE = {
     GRAYSCALE: false,
     SEPIA: false,
-    CONTRAST: false
+    CONTRAST: false,
+    BLUR: false
   }
+
+  let uBlurSigma = 5.0
 
   const FILTER_UNIFORM_VALUES = {
     [FILTER_MODE.CONTRAST]: {
@@ -36,29 +41,47 @@
     }
   }
 
-  let ACTIVE_FILTERS: number[] = []
+  type ValueOf<T> = T[keyof T]
+  let ACTIVE_FILTERS: ValueOf<typeof FILTER_MODE>[] = []
 
-  const onChange = (target: FILTER) => (on: boolean) => {
-    if (on) {
-      ACTIVE_FILTERS.push(FILTER_MODE[target])
-    } else {
-      const index = ACTIVE_FILTERS.indexOf(FILTER_MODE[target])
-      if (index > -1) {
-        ACTIVE_FILTERS.splice(index, 1)
+  const onChange =
+    (...target: FILTER[]) =>
+    (on: boolean) => {
+      if (on) {
+        for (const filter of target) {
+          if (ACTIVE_FILTERS.indexOf(FILTER_MODE[filter]) === -1) {
+            ACTIVE_FILTERS.push(FILTER_MODE[filter])
+          }
+        }
+      } else {
+        for (const filter of target) {
+          const index = ACTIVE_FILTERS.indexOf(FILTER_MODE[filter])
+          if (index > -1) {
+            ACTIVE_FILTERS.splice(index, 1)
+          }
+        }
       }
     }
-  }
 
   onMount(async () => {
     // client side only
     const { SketchFilter } = await import("sketchgl")
 
     const sketch: FilterSketchFn = ({ gl, canvas, fitImage }) => {
-      const uniforms = new Uniforms(gl, ["uFilterMode", "uContrastGamma"])
+      const uniforms = new Uniforms(gl, ["uFilterMode", "uContrastGamma", "uBlurSigma"])
 
       const APPLY_UNIFORMS = {
+        [FILTER_MODE.NONE]: null,
+        [FILTER_MODE.GRAYSCALE]: null,
+        [FILTER_MODE.SEPIA]: null,
         [FILTER_MODE.CONTRAST]: () => {
           uniforms.float("uContrastGamma", FILTER_UNIFORM_VALUES[FILTER_MODE.CONTRAST].uGamma)
+        },
+        [FILTER_MODE.BLUR_X]: () => {
+          uniforms.float("uBlurSigma", uBlurSigma)
+        },
+        [FILTER_MODE.BLUR_Y]: () => {
+          uniforms.float("uBlurSigma", uBlurSigma)
         }
       }
 
@@ -142,4 +165,10 @@
     max={10}
     step={0.1}
   />
+</div>
+<div>
+  <Checkbox bind:on={FILTER_ENABLE_STATE.BLUR} onChange={onChange("BLUR_X", "BLUR_Y")}>
+    Blur
+  </Checkbox>
+  <Slider bind:value={uBlurSigma} disabled={!FILTER_ENABLE_STATE.BLUR} min={1} max={5} step={0.5} />
 </div>

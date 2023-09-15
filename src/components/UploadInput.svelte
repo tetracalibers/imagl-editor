5fb9445ee04c992e9752a6d2a86b79eb84dbe686
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-09-16 06:52:29 +0900

    UploadInput: ファイル選択欄を仮組み

---
+++ src/components/UploadInput.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let onChange: (file: File) => void
+
+  let files: FileList
+
+  $: if (files) {
+    onChange(files[0])
+  }
+</script>
+
+<input type="file" accept="image/png, image/jpeg" bind:files />

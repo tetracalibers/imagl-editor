d8f2a90bae1eaa74cb2340813ea3e7aa474a712f
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-09-16 06:52:04 +0900

    eslintrc: いくつかの警告をoffに

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,7 +9,8 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   rules: {
-    "@typescript-eslint/no-empty-function": "off"
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-explicit-any": "off"
   },
   parserOptions: {
     sourceType: "module",

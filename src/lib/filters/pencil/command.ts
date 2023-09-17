44a592dc83a16a12a37202c0704351a52b71a360
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-09-17 09:22:30 +0900

    localRenderer => mrtRenderer

--- src/lib/filters/pencil/command.ts
+++ src/lib/filters/pencil/command.ts
@@ -8,7 +8,7 @@ import type { CanvasCoverPolygon } from "sketchgl/geometry"
 export class PencilFilter {
   private _gl: WebGL2RenderingContext
   private _localUniforms: Uniforms<"uPencilGamma">
-  private _localRenderer: UseMRT
+  private _mrtRenderer: UseMRT
   private _localProgram: Program
   private _screen: CanvasCoverPolygon
 
@@ -18,17 +18,17 @@ export class PencilFilter {
     this._gl = gl
     this._screen = screen
 
-    this._localRenderer = new UseMRT(gl, canvas, vert, frag_1, { texCount: 2, texUnitStart: 1 })
+    this._mrtRenderer = new UseMRT(gl, canvas, vert, frag_1, { texCount: 2, texUnitStart: 1 })
 
     this._localProgram = new Program(gl)
     this._localProgram.attach(vert, frag_2)
 
     this._localUniforms = new Uniforms(gl, ["uPencilGamma"])
-    this._localUniforms.init(this._localRenderer.glProgramForMTR)
+    this._localUniforms.init(this._mrtRenderer.glProgramForMTR)
   }
 
   apply(out: WebGLFramebuffer | null) {
-    const r = this._localRenderer
+    const r = this._mrtRenderer
     const program = this._localProgram
 
     r.switchToMTR()
@@ -36,8 +36,8 @@ export class PencilFilter {
     this._screen.draw({ primitive: "TRIANGLES" })
 
     r.switchToNextTexture(program, out)
-    this._localRenderer.useTexture(program, { idx: 0, name: "uPosterizeTex" })
-    this._localRenderer.useTexture(program, { idx: 1, name: "uEdgeTex" })
+    this._mrtRenderer.useTexture(program, { idx: 0, name: "uPosterizeTex" })
+    this._mrtRenderer.useTexture(program, { idx: 1, name: "uEdgeTex" })
     this._screen.draw({ primitive: "TRIANGLES" })
   }
 
@@ -50,10 +50,10 @@ export class PencilFilter {
   }
 
   get resize() {
-    return this._localRenderer.resize
+    return this._mrtRenderer.resize
   }
 
   get glProgram() {
-    return this._localRenderer.glProgramForMTR
+    return this._mrtRenderer.glProgramForMTR
   }
 }

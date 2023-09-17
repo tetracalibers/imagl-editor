8d9844ba8070f4e374982762e11c3b697ea5c1db
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-09-17 17:39:21 +0900

    不要なメソッド削除

--- src/lib/filters/blur/command.ts
+++ src/lib/filters/blur/command.ts
@@ -1,4 +1,4 @@
-import { getObjectKeys, type FilterCommand } from "./../filter-stack"
+import type { FilterCommand } from "./../filter-stack"
 import type { Uniforms } from "sketchgl/program"
 
 type U = "uBlurSigma"
@@ -17,10 +17,6 @@ export class BlurFilter implements FilterCommand<U> {
   applyUniforms(uniforms: Uniforms<U | "uFilterMode">) {
     uniforms.int("uFilterMode", this.modeIdx)
     uniforms.float("uBlurSigma", this.uniforms.uBlurSigma)
-  }
-
-  get uniformNames() {
-    return getObjectKeys(this.uniforms)
   }
 
   get uBlurSigma() {

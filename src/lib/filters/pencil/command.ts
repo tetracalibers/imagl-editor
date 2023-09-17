58120599a38a57c533c975dda133111e4be1be68
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-09-17 10:58:55 +0900

    鉛筆画風加工後にblurフィルタを連鎖させられるように

--- src/lib/filters/pencil/command.ts
+++ src/lib/filters/pencil/command.ts
@@ -4,11 +4,13 @@ import vert from "../../shaders/image.vert?raw"
 import frag_1 from "./1.frag?raw"
 import frag_2 from "./2.frag?raw"
 import type { CanvasCoverPolygon } from "sketchgl/geometry"
+import { UseFramebuffer } from "$lib/core/offscreen"
 
 export class PencilFilter {
   private _gl: WebGL2RenderingContext
   private _path1Uniforms: Uniforms<"uPencilGamma">
   private _mrtRenderer: UseMRT
+  private _offRenderer: UseFramebuffer
   private _path2Program: Program
   private _screen: CanvasCoverPolygon
 
@@ -19,6 +21,7 @@ export class PencilFilter {
     this._screen = screen
 
     this._mrtRenderer = new UseMRT(gl, canvas, vert, frag_1, { texCount: 2, texUnitStart: 1 })
+    this._offRenderer = new UseFramebuffer(gl, canvas, { texUnitStart: 3 })
 
     this._path2Program = new Program(gl)
     this._path2Program.attach(vert, frag_2)
@@ -27,17 +30,22 @@ export class PencilFilter {
     this._path1Uniforms.init(this._mrtRenderer.glProgramForMTR)
   }
 
-  apply(out: WebGLFramebuffer | null) {
-    const r = this._mrtRenderer
+  apply(outProgram: Program, outFBuffer: WebGLFramebuffer | null) {
+    const mrtR = this._mrtRenderer
+    const offR = this._offRenderer
     const path2Program = this._path2Program
 
-    r.switchToMTR()
+    mrtR.switchToMTR()
     this._path1Uniforms.float("uPencilGamma", this._uPencilGamma)
     this._screen.draw({ primitive: "TRIANGLES" })
 
-    r.switchToNextTexture(path2Program, out)
-    this._mrtRenderer.useTexture(path2Program, { idx: 0, name: "uPosterizeTex" })
-    this._mrtRenderer.useTexture(path2Program, { idx: 1, name: "uEdgeTex" })
+    mrtR.switchToNextTexture(path2Program, offR.framebuffer)
+    mrtR.useTexture(path2Program, { idx: 0, name: "uPosterizeTex" })
+    mrtR.useTexture(path2Program, { idx: 1, name: "uEdgeTex" })
+    this._screen.draw({ primitive: "TRIANGLES" })
+
+    offR.switchToNextTexture(outProgram, outFBuffer)
+    offR.useTexture(outProgram, { name: "uMainTex" })
     this._screen.draw({ primitive: "TRIANGLES" })
   }
 
@@ -49,8 +57,8 @@ export class PencilFilter {
     return this._uPencilGamma
   }
 
-  get resize() {
-    return this._mrtRenderer.resize
+  get resizes() {
+    return [this._offRenderer.resize, this._mrtRenderer.resize]
   }
 
   get glProgram() {
--- src/routes/pencil/Canvas.svelte
+++ src/routes/pencil/Canvas.svelte
@@ -11,6 +11,9 @@
   import DownloadButton from "../../components/DownloadButton.svelte"
   import UploadInput from "../../components/UploadInput.svelte"
   import Slider from "$lib/components/control/Slider.svelte"
+  import { FilterStack } from "$lib/filters/filter-stack"
+  import { BlurFilter } from "$lib/filters/blur/command"
+  import Checkbox from "$lib/components/control/Checkbox.svelte"
 
   let canvas: HTMLCanvasElement
   let download: () => void
@@ -21,9 +24,17 @@
   let mainFilter: PencilFilter
   let uPencilGamma: number
 
+  const blurX = new BlurFilter({ modeIdx: 1 })
+  const blurY = new BlurFilter({ modeIdx: 2 })
+
+  const filterStack = new FilterStack({ blurX, blurY })
+
   const sketch: SketchFn = ({ gl, canvas }) => {
     const programForOptions = new Program(gl)
     programForOptions.attach(vert, frag_options)
+    programForOptions.activate()
+
+    const uniforms = filterStack.initUniforms(gl, programForOptions.glProgram)
 
     const plane = new CanvasCoverPolygon(gl)
     plane.setLocations({ vertices: 0, uv: 1 })
@@ -41,16 +52,25 @@
         stackRenderer.init(texture)
       },
       drawOnFrame() {
+        uniforms && uniforms.int("uFilterMode", 0)
+
         plane.bind()
         stackRenderer.bind(mainFilter.glProgram, "uOriginalTex")
 
         const { outBuf } = stackRenderer.beginPath()
-        mainFilter.apply(outBuf)
+        mainFilter.apply(programForOptions, outBuf)
+
+        filterStack.activeFilters.forEach((filter) => {
+          stackRenderer.beginPath()
+          uniforms && filter.applyUniforms(uniforms)
+          plane.draw({ primitive: "TRIANGLES" })
+          stackRenderer.endPath()
+        })
 
         stackRenderer.switchToCanvas()
         plane.draw({ primitive: "TRIANGLES" })
       },
-      resize: [mainFilter.resize]
+      resize: [...mainFilter.resizes, stackRenderer.resize]
     }
   }
 
@@ -80,5 +100,33 @@
     min={0}
     max={1}
     step={0.01}
+  />
+</div>
+
+<div>
+  <Checkbox
+    bind:on={blurX.active}
+    onChange={(on) => {
+      if (on) {
+        filterStack.active("blurX")
+        filterStack.active("blurY")
+      } else {
+        filterStack.deactive("blurX")
+        filterStack.deactive("blurY")
+      }
+    }}
+  >
+    Blur
+  </Checkbox>
+  <Slider
+    bind:value={blurX.uBlurSigma}
+    onChange={(v) => {
+      blurX.uBlurSigma = v
+      blurY.uBlurSigma = v
+    }}
+    disabled={!blurX.active}
+    min={1}
+    max={5}
+    step={0.5}
   />
 </div>

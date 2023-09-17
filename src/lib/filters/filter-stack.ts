7125773f0a6b1b55e8d2ac4f07ab5d3660e1615d
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-09-17 10:56:35 +0900

    FilterStack: 同じprogramを共有する複数フィルタの管理クラス

---
+++ src/lib/filters/filter-stack.ts
@@ -0,0 +1,61 @@
+import { Uniforms } from "sketchgl/program"
+
+export interface FilterCommand<U extends string> {
+  modeIdx: number
+  active: boolean
+  applyUniforms(uniforms: Uniforms<U | "uFilterMode">): void
+  uniforms: Record<U, number>
+}
+
+export const getObjectKeys = <T extends { [key: string]: unknown }>(obj: T): (keyof T)[] => {
+  return Object.keys(obj)
+}
+
+export class FilterStack<C extends string, U extends string> {
+  private _commands: Map<C, FilterCommand<U>>
+  private _actives: Map<C, FilterCommand<U>>
+  private _uniforms?: Set<U>
+
+  constructor(commands: Record<C, FilterCommand<U>>) {
+    this._commands = new Map()
+    getObjectKeys(commands).forEach((key) => {
+      this._commands.set(key, commands[key])
+    })
+
+    this._uniforms = new Set<U>()
+    this._commands.forEach((command) => {
+      getObjectKeys(command.uniforms).forEach((name) => {
+        this._uniforms?.add(name)
+      })
+    })
+
+    this._actives = new Map()
+  }
+
+  initUniforms(gl: WebGL2RenderingContext, program: WebGLProgram | null) {
+    if (!this._uniforms) return
+    const uniforms = new Uniforms(gl, ["uFilterMode", ...this._uniforms])
+    uniforms.init(program)
+    return uniforms
+  }
+
+  active(id: C) {
+    const command = this._commands.get(id)
+    if (command) {
+      command.active = true
+      this._actives.set(id, command)
+    }
+  }
+
+  deactive(id: C) {
+    const command = this._commands.get(id)
+    if (command) {
+      command.active = false
+      this._actives.delete(id)
+    }
+  }
+
+  get activeFilters() {
+    return this._actives
+  }
+}

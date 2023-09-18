09c5a695fcc254fd40085f02f11b3cd99092e68c
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-09-18 13:42:04 +0900

    Checkbox: disabled指定を可能に

--- src/lib/components/control/Checkbox.svelte
+++ src/lib/components/control/Checkbox.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
   export let on = false
   export let onChange: (on: boolean) => void = () => {}
+  export let disabled = false
 </script>
 
 <label>
-  <input type="checkbox" bind:checked={on} on:change={(e) => onChange(e.currentTarget.checked)} />
+  <input
+    type="checkbox"
+    bind:checked={on}
+    on:change={(e) => onChange(e.currentTarget.checked)}
+    {disabled}
+  />
   <slot />
 </label>

da8292fd629bf9dfaa75b2b15cd9289e223c240a
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-09-18 11:28:23 +0900

    目次を更新

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,6 @@
   <li><a href="/normal">オリジナル</a></li>
   <li><a href="/watercolor-pencil">水彩色鉛筆画</a></li>
   <li><a href="/pencil">鉛筆画</a></li>
-  <li><a href="/crayon">クレヨン画</a></li>
   <li><a href="/spray">スプレー画</a></li>
   <li><a href="/stained-glass">ステンドグラス</a></li>
   <li><a href="/watercolor">水彩画</a></li>

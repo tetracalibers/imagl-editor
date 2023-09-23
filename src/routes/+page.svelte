a51b4e30747c0fbf2a55a7c3363e64407b409ec9
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-09-24 08:40:48 +0900

    目次更新

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,7 @@
   <li><a href="/mosaic">モザイク</a></li>
   <li><a href="/pencil">鉛筆によるデッサン画</a></li>
   <li><a href="/color-pencil">色鉛筆画</a></li>
+  <li><a href="/pale-color-pencil">淡い色鉛筆画（エッジが細かい画像向け）</a></li>
   <li><a href="/spray">スプレー画</a></li>
   <li><a href="/stained-glass">ステンドグラス</a></li>
   <li><a href="/stained-glass-2">ステンドグラス2</a></li>

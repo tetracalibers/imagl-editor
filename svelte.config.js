715f121ce4215eb10b884a0ef8cdb590f91ed153
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-10-02 07:40:20 +0900

    デプロイスクリプトとビルド設定追加

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from "@sveltejs/adapter-static"
 import { vitePreprocess } from "@sveltejs/kit/vite"
 
+const dev = process.argv.includes("dev")
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -16,7 +18,10 @@ const config = {
       fallback: undefined,
       precompress: false,
       strict: true
-    })
+    }),
+    paths: {
+      base: dev ? "" : "/imagl-editor"
+    }
   }
 }
 

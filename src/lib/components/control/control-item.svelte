8f7ab81f85af382190084c9057635a682d7fea04
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-10-01 22:55:34 +0900

    UIとラベルのレイアウトを担うコンポーネント

---
+++ src/lib/components/control/control-item.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  export let title: string
+</script>
+
+<div class="control-item">
+  <h2 class="title">{title}</h2>
+  <slot />
+</div>
+
+<style>
+  .control-item {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .title {
+    margin: 0;
+  }
+</style>

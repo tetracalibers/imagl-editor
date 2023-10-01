3233d344dbe6109ec70d53009c8c4449dbe4d8e3
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-10-01 18:19:35 +0900

    ダウンロードボタンのスタイルを調整

--- src/components/DownloadButton.svelte
+++ src/components/DownloadButton.svelte
@@ -3,4 +3,16 @@
   export let disabled = false
 </script>
 
-<button on:click={onClick} {disabled}>Download</button>
+<button class="download-button" on:click={onClick} {disabled}>Download</button>
+
+<style>
+  .download-button {
+    --button-color: #ef9595;
+    appearance: none;
+    border: 0;
+    border-radius: 10px;
+    background-color: var(--button-color);
+    box-shadow: var(--button-color) 0 10px 20px -10px;
+    padding: 0.5em 1em;
+  }
+</style>

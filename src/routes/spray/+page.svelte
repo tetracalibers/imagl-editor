83b22eacb5be86dfa96e2f3a8e4c8b91ca34e0ec
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-10-02 17:37:09 +0900

    不要なページを削除

--- src/routes/spray/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  import Canvas from "./Canvas.svelte"
-</script>
-
-<h1>スプレー画</h1>
-<Canvas />
-
-<a href="https://tetracalibers.github.io/webgl-practice-ground/crayon-spray-image/">sample</a>
